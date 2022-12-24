f2ab11ddc9da2a3624888e19b45fa955ecdc3808
Alexandr Naum <mfq07k9cz@gmail.com>
2022-12-24 14:16:24 +0300

    remove post, editing post

--- src/redux/slice/posts.js
+++ src/redux/slice/posts.js
@@ -11,6 +11,10 @@ export const fetchTags = createAsyncThunk('posts/fethTags', async () => {
   return data
 })
 
+export const fetchRemove = createAsyncThunk('posts/fethRemove', async id => {
+  await axios.delete(`/posts/${id}`)
+})
+
 const initialState = {
   posts: {
     items: [],
@@ -27,6 +31,7 @@ const postsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
+    //Получение статей
     [fetchPosts.pending]: state => {
       state.posts.items = []
       state.posts.status = 'loading'
@@ -39,6 +44,7 @@ const postsSlice = createSlice({
       state.posts.items = []
       state.posts.status = 'error'
     },
+    //Получение тегов
     [fetchTags.pending]: state => {
       state.tags.items = []
       state.tags.status = 'loading'
@@ -50,6 +56,10 @@ const postsSlice = createSlice({
     [fetchTags.rejected]: state => {
       state.tags.items = []
       state.tags.status = 'error'
+    },
+    //Удаление статей
+    [fetchRemove.pending]: (state, action) => {
+      state.posts.items = state.posts.items.filter(obj => obj._id !== action.meta.arg)
     }
   }
 })
