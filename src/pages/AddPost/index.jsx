33d8479dc4f525a462bdbe2823f0fa3cadcce243
Alexandr Naum <mfq07k9cz@gmail.com>
2022-12-24 10:44:14 +0300

    added creation of post, tags and title

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import SimpleMDE from 'react-simplemde-editor';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+import TextField from '@mui/material/TextField'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import SimpleMDE from 'react-simplemde-editor'
+import { selectIsAuth } from '../../redux/slice/auth'
 
-import 'easymde/dist/easymde.min.css';
-import styles from './AddPost.module.scss';
+import 'easymde/dist/easymde.min.css'
+import styles from './AddPost.module.scss'
 
 export const AddPost = () => {
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
+  const isAuth = useSelector(selectIsAuth)
+  const imageUrl = ''
+  const [value, setValue] = React.useState('')
+  const [title, setTitle] = React.useState('')
+  const [tags, setTags] = React.useState('')
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = () => {}
 
-  const onClickRemoveImage = () => {};
+  const onClickRemoveImage = () => {}
 
-  const onChange = React.useCallback((value) => {
-    setValue(value);
-  }, []);
+  const onChange = React.useCallback(value => {
+    setValue(value)
+  }, [])
 
   const options = React.useMemo(
     () => ({
@@ -28,44 +34,55 @@ export const AddPost = () => {
       status: false,
       autosave: {
         enabled: true,
-        delay: 1000,
-      },
+        delay: 1000
+      }
     }),
-    [],
-  );
+    []
+  )
+
+  if (!window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to='/' />
+  }
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button variant='outlined' size='large'>
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input type='file' onChange={handleChangeFile} hidden />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+        <Button variant='contained' color='error' onClick={onClickRemoveImage}>
           Удалить
         </Button>
       )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
-      )}
+      {imageUrl && <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt='Uploaded' />}
       <br />
       <br />
       <TextField
         classes={{ root: styles.title }}
-        variant="standard"
-        placeholder="Заголовок статьи..."
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        variant='standard'
+        placeholder='Заголовок статьи...'
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
+      <TextField
+        classes={{ root: styles.tags }}
+        value={tags}
+        onChange={e => setTags(e.target.value)}
+        variant='standard'
+        placeholder='Тэги'
+        fullWidth
+      />
       <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
+        <Button size='large' variant='contained'>
           Опубликовать
         </Button>
-        <a href="/">
-          <Button size="large">Отмена</Button>
+        <a href='/'>
+          <Button size='large'>Отмена</Button>
         </a>
       </div>
     </Paper>
-  );
-};
+  )
+}
